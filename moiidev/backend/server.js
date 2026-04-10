1dc00bf6fb29588b178584f1b59635bfe3290c3c
Devendra Kumar <dev2111moi@gmail.com>
2026-04-10 10:41:30 +0530

    Update CORS configuration to allow multiple origins

--- moiidev/backend/server.js
+++ moiidev/backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173','https://erpmoiidev.vercel.app/login'], credentials: true }));
 app.use(express.json());
 
 // Routes
